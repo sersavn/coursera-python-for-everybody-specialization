bfdadc58bc61162cfbaae136edb1a920e85154b5
Sergei Saprunov <nooooooooob@yandex.ru>
2017-10-28 04:54:58 +0300

    message

--- assignment_15_3/assignment_15_3.py
+++ assignment_15_3/assignment_15_3.py
@@ -30,7 +30,7 @@ CREATE TABLE Member (
 
 fname = input('Enter file name: ')
 if len(fname) < 1:
-    fname = 'roster_data_sample.json'
+    fname = 'roster_data.json'
 
 # [
 #   [ "Charley", "si110", 1 ],
@@ -43,8 +43,9 @@ for entry in json_data:
 
     name = entry[0];
     title = entry[1];
+    role = entry[2];
 
-    print((name, title))
+    print((name, title, role))
 
     cur.execute('''INSERT OR IGNORE INTO User (name)
         VALUES ( ? )''', ( name, ) )
@@ -57,7 +58,7 @@ for entry in json_data:
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id) VALUES ( ?, ? )''',
-        ( user_id, course_id ) )
+        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
+        ( user_id, course_id, role ) )
 
     conn.commit()
